44c40442cc3b4a36c58ee44e664cd9207a94ed25
Ekaterina Grishina <grishinaekateina@gmail.com>
2024-10-28 15:39:35 +0300

    first commit

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -5,6 +5,7 @@ import math
 # ## Task 0.1
 from typing import Callable, Iterable
 
+
 #
 # Implementation of a prelude of elementary functions.
 
@@ -31,8 +32,74 @@ from typing import Callable, Iterable
 # For is_close:
 # $f(x) = |x - y| < 1e-2$
 
+def mul(a, b):
+    return a * b
 
-# TODO: Implement for Task 0.1.
+
+def id(a):
+    return a
+
+
+def add(a, b):
+    return a + b
+
+
+def neg(a):
+    return - a
+
+
+def lt(a, b):
+    return a < b
+
+
+def eq(a, b):
+    return a == b
+
+
+def max(a, b):
+    if a >= b:
+        return a
+    return b
+
+
+def is_close(a, b):
+    return abs(a - b) < 1e-2
+
+
+def sigmoid(a):
+    if a >= 0:
+        return 1 / (1 + math.exp(-a))
+    return math.exp(a) / (1 + math.exp(a))
+
+
+def relu(a):
+    return max(a, 0)
+
+
+def log(a):
+    return math.log(a)
+
+
+def exp(a):
+    return math.exp(a)
+
+
+def inv(a):
+    return 1 / a
+
+
+def log_back(a, b):
+    return b / a
+
+
+def inv_back(a, b):
+    return -b / a ** 2
+
+
+def relu_back(a, b):
+    if a > 0:
+        return b
+    return 0
 
 
 # ## Task 0.3
@@ -51,4 +118,32 @@ from typing import Callable, Iterable
 # - prod: take the product of lists
 
 
-# TODO: Implement for Task 0.3.
+def map(f, lst):
+    return [f(lst[i]) for i in range(len(lst))]
+
+
+def zipWith(f, lst1, lst2):
+    return [f(lst1[i], lst2[i]) for i in range(len(lst1))]
+
+
+def reduce(f, lst):
+    a = f(lst[0], lst[1])
+    for i in range(2, len(lst)):
+        a = f(a, lst[i])
+    return a
+
+
+def negList(lst):
+    return map(neg, lst)
+
+
+def addLists(lst1, lst2):
+    return zipWith(add, lst1, lst2)
+
+
+def sum(lst):
+    return reduce(add, lst)
+
+
+def prod(lst):
+    return reduce(mul, lst)
--- tests/test_operators.py
+++ tests/test_operators.py
@@ -27,6 +27,7 @@ from minitorch.operators import (
 
 from .strategies import assert_close, small_floats
 
+
 # ## Task 0.1 Basic hypothesis tests.
 
 
@@ -107,16 +108,17 @@ def test_sigmoid(a: float) -> None:
     * It crosses 0 at 0.5
     * It is  strictly increasing.
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert 0.0 <= sigmoid(a) <= 1.0
+    assert_close(1.0 - sigmoid(a), sigmoid(-a))
+    assert_close(sigmoid(0.0), 0.5)
+    assert sigmoid(a) < sigmoid(a + 1e-2)
 
 
 @pytest.mark.task0_2
 @given(small_floats, small_floats, small_floats)
 def test_transitive(a: float, b: float, c: float) -> None:
     """Test the transitive property of less-than (a < b and b < c implies a < c)"""
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert (lt(a, b) and lt(b, c) and lt(a, c)) or not (lt(a, b) and lt(b, c))
 
 
 @pytest.mark.task0_2
@@ -124,8 +126,9 @@ def test_symmetric() -> None:
     """Write a test that ensures that :func:`minitorch.operators.mul` is symmetric, i.e.
     gives the same value regardless of the order of its input.
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    for a in range(-5, 5):
+        for b in range(-5, 5):
+            assert mul(a, b) == mul(b, a)
 
 
 @pytest.mark.task0_2
@@ -133,15 +136,16 @@ def test_distribute() -> None:
     r"""Write a test that ensures that your operators distribute, i.e.
     :math:`z \times (x + y) = z \times x + z \times y`
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    for x in range(-5, 5):
+        for y in range(-5, 5):
+            for z in range(-2, 2):
+                assert mul(z, add(x, y))== add(mul(z, x), mul(z, y))
 
 
 @pytest.mark.task0_2
 def test_other() -> None:
     """Write a test that ensures some other property holds for your functions."""
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert_close(inv(inv(5.0)), 5.0)
 
 
 # ## Task 0.3  - Higher-order functions
@@ -168,8 +172,7 @@ def test_sum_distribute(ls1: List[float], ls2: List[float]) -> None:
     """Write a test that ensures that the sum of `ls1` plus the sum of `ls2`
     is the same as the sum of each element of `ls1` plus each element of `ls2`.
     """
-    # TODO: Implement for Task 0.3.
-    raise NotImplementedError("Need to implement for Task 0.3")
+    assert_close(sum(ls1) + sum(ls2), addLists(ls1, ls2))
 
 
 @pytest.mark.task0_3
